b30c6c8a8ae29ce631299d4555755efdc9e26a7b
kdushyant02 <39339430+kdushyant02@users.noreply.github.com>
2018-09-26 10:10:42 +0000

    Update slackNotifier.groovy
    added channel string in code

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult) {
+def call(String buildResult,string SLACK_CHANNEL) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful - ${env.BUILD_URL}"
+    slackSend color: "good", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful - ${env.BUILD_URL}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed - ${env.BUILD_URL}"
+    slackSend color: "danger", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed - ${env.BUILD_URL}"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable - ${env.BUILD_URL}"
+    slackSend color: "warning", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable - ${env.BUILD_URL}"
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear - ${env.BUILD_URL}"	
+    slackSend color: "danger", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear - ${env.BUILD_URL}"	
   }
 }
