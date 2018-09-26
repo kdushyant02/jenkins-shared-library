929533dc1cb91d4528a91a7a8ec26bf93e2263d0
kdushyant02 <39339430+kdushyant02@users.noreply.github.com>
2018-09-26 10:26:32 +0000

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,15 +2,19 @@
 
 def call(String buildResult,String SLACK_CHANNEL) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful - ${env.BUILD_URL}"
+    slackSend color: "good", channel: "${params.SLACK_CHANNEL}", message: "*SUCCESS:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${env.USER_ID}\n More info at: ${env.BUILD_URL}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed - ${env.BUILD_URL}"
+    slackSend color: "danger", channel: "${params.SLACK_CHANNEL}", message: "*FAILED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${env.USER_ID}\n More info at: ${env.BUILD_URL}"
+  }
+  else if( buildResult == "ABORTED" ) { 
+    slackSend color: "warning", channel: "${params.SLACK_CHANNEL}", message: "*ABORTED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${env.USER_ID}\n More info at: ${env.BUILD_URL}"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable - ${env.BUILD_URL}"
+    slackSend color: "warning", channel: "${params.SLACK_CHANNEL}", message: "*UNSTABLE:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${env.USER_ID}\n More info at: ${env.BUILD_URL}"
   }
-  else {
+  else 
+  {
     slackSend color: "danger", channel: "${params.SLACK_CHANNEL}", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear - ${env.BUILD_URL}"	
   }
 }
