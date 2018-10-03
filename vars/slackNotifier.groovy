4058a89facc616e61c074423a6970e61844ad08d
kdushyant02 <39339430+kdushyant02@users.noreply.github.com>
2018-10-03 13:45:09 +0000

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -5,16 +5,16 @@ def call(String buildResult, String SLACK_CHANNEL) {
     slackSend (color: "good", channel: "${params.SLACK_CHANNEL}", message: "*STARTED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}.\n More info at: ${env.BUILD_URL}")
   }
   else if ( buildResult == "SUCCESS" ) {
-    slackSend (color: "good", channel: "${params.SLACK_CHANNEL}", message: "*SUCCESS:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} /* by ${env.USER_ID} */ \n More info at: ${env.BUILD_URL}")
+    slackSend (color: "good", channel: "${params.SLACK_CHANNEL}", message: "*SUCCESS:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}. \n More info at: ${env.BUILD_URL}")
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend (color: "danger", channel: "${params.SLACK_CHANNEL}", message: "*FAILED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${env.USER_ID}\n More info at: ${env.BUILD_URL}")
+    slackSend (color: "danger", channel: "${params.SLACK_CHANNEL}", message: "*FAILED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}. \n More info at: ${env.BUILD_URL}")
   }
   else if( buildResult == "ABORTED" ) { 
-    slackSend (color: "warning", channel: "${params.SLACK_CHANNEL}", message: "*ABORTED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${env.USER_ID}\n More info at: ${env.BUILD_URL}")
+    slackSend (color: "warning", channel: "${params.SLACK_CHANNEL}", message: "*ABORTED:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}. \n More info at: ${env.BUILD_URL}")
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend (color: "warning", channel: "${params.SLACK_CHANNEL}", message: "*UNSTABLE:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${env.USER_ID}\n More info at: ${env.BUILD_URL}")
+    slackSend (color: "warning", channel: "${params.SLACK_CHANNEL}", message: "*UNSTABLE:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}. \n More info at: ${env.BUILD_URL}")
   }
   else 
   {
